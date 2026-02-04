bfe934302aa19572a280f1a57c76fb750d86d527
harisonns09 <harisonns09@gmail.com>
2026-02-04 15:45:08 -0300

    ajuste baseurl

--- services/api.ts
+++ services/api.ts
@@ -4,7 +4,7 @@ import { Member, Transaction, Event, Ministry, Scale, SmallGroup, PrayerRequest,
 const api = axios.create({
   // O endereço onde seu Spring Boot está rodando
   // baseURL: 'http://localhost:8080', 
-  baseURL: 'https://gen-lang-client-0788356664.rj.r.appspot.com/api',
+  baseURL: 'https://gen-lang-client-0788356664.rj.r.appspot.com',
 });
 
 // Interceptador para adicionar o Token automaticamente em toda requisição
